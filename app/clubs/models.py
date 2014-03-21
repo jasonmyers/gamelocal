e1baed4561fb1f960791cb7d45d5201b6d436e56
Jason Myers <j.myers@brstrat.com>
2014-03-21 08:51:13 -0400

    Add polymorphic models for Go and Chess clubs

--- app/clubs/models.py
+++ app/clubs/models.py
@@ -18,5 +18,17 @@ class Club(BaseModel):
     name = db.Column(db.Unicode(500))
     game = db.Column(UnicodeChoices(50, choices=CLUB_GAME_CHOICES))
 
+    __mapper_args__ = {'polymorphic_on': game}
+
     def __unicode__(self):
         return 'Club {}'.format(self.name)
+
+
+class GoClub(Club):
+    __tablename__ = None
+    __mapper_args__ = {'polymorphic_identity': 'go'}
+
+
+class ChessClub(Club):
+    __tablename__ = None
+    __mapper_args__ = {'polymorphic_identity': 'chess'}
